4aee3cc73a372e708c291a560f0327fa8f089ec2
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2025-05-26 23:58:03 +0100

    Make changes to Banne.jsx vh.

--- src/app/home/components/Banner.jsx
+++ src/app/home/components/Banner.jsx
@@ -13,8 +13,27 @@ const Banner = () => {
     return () => clearInterval(timeoutRef.current)
   }, [])
 
+  useEffect(() => {
+    const setVH = () => {
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }
+
+    setVH()
+    window.addEventListener('resize', setVH)
+    return () => window.removeEventListener('resize', setVH)
+  })
+
   return (
-    <div className="banner" style={{ position: "relative", width: "100%", height: "100vh", overflow: "hidden" }}>
+    <div 
+      className="banner" 
+      style={{ 
+        position: "relative",
+        width: "100%", 
+        height: "calc(var(--vh, 1vh) * 100)", 
+        overflow: "hidden" 
+        }}
+      >
       {/* SLIDE ONE */}
       <div
         className="slide slide-one"
