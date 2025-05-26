0de1e307952d3bba2617c56691d7bf3dc8517444
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2025-05-27 00:01:55 +0100

    Make changes to vh for Banner.jsx.

--- src/app/home/components/Banner.jsx
+++ src/app/home/components/Banner.jsx
@@ -13,24 +13,13 @@ const Banner = () => {
     return () => clearInterval(timeoutRef.current)
   }, [])
 
-  useEffect(() => {
-    const setVH = () => {
-      const vh = window.innerHeight * 0.01
-      document.documentElement.style.setProperty('--vh', `${vh}px`)
-    }
-
-    setVH()
-    window.addEventListener('resize', setVH)
-    return () => window.removeEventListener('resize', setVH)
-  })
-
   return (
     <div 
       className="banner" 
       style={{ 
         position: "relative",
         width: "100%", 
-        height: "calc(var(--vh, 1vh) * 100)", 
+        height: "720px", 
         overflow: "hidden" 
         }}
       >
