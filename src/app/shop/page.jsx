184a2ab937fea3623bcb49b4e34469cd8302659c
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2025-06-21 19:25:11 +0100

    Create conditional logic inside of /app/shop/page.jsx for when shop is read or not.

--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,23 +1,33 @@
 export const dynamic = 'force-dynamic';
 
+import Header from './components/Header';
 import React from 'react';
-// import Header from './components/Header';
-// import Products from './components/Products';
+import Products from './components/Products';
 import ShopMaintenance from './components/ShopMaintenance';
 import '@/styles/shop.css';
 
-// const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
 
-/* const getProducts = async () => {
+const getProducts = async () => {
   const res = await fetch(`${baseUrl}/api/new-shopify/storefront/products?nocache=${Date.now()}`, {
     cache: 'no-store',
   });
   if (!res.ok) throw new Error('Failed to fetch products');
   return await res.json();
-}; */
+}; 
+
+const SHOP_READY = process.env.SHOP_READY === 'true';
 
 export default async function Page() {
- /* let products = null;
+  if (!SHOP_READY) {
+    return (
+      <div className='shop-container'>
+        <ShopMaintenance />
+      </div>
+    );
+  }
+
+  let products = null;
   let error = null;
 
   try {
@@ -26,16 +36,15 @@ export default async function Page() {
   } catch (err) {
     console.error(err);
     error = err.message || "An error occurred";
-  }*/
+  }
 
   return (
     <div className='shop-container'>
-      <ShopMaintenance />
-      {/*<Header />
+      <Header />
       <Products 
         products={products}
         error={error}
-      /> */}
+      />
     </div>
   );
 }
