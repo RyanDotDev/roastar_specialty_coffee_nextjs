1516eb0d3dae181a5222eff7d820771ad12635e9
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2025-07-19 06:24:58 +0100

    Restructure app router by adding /confirmation/[intentId] inside of /checkout.

--- src/app/checkout/confirmation/page.jsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic'
-
-const CheckoutReturnRedirect = dynamic(() => import('./RedirectClient'), {
-  ssr: false,
-})
-
-export default function RedirectPage() {
-  return <CheckoutReturnRedirect />
-}
